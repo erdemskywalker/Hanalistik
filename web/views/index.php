c0aef9a5c0a3605a26e92f629a50ae39232abc2c
Yusuf Erdem Turan <76514562+erdemskywalker@users.noreply.github.com>
2026-06-03 22:07:07 +0300

    Update index.php

--- web/views/index.php
+++ web/views/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TuAnalytics</title>
+    <title>Hanalistik</title>
     <link rel="stylesheet" href="extras/bootstrap.min.css">
     <script src="extras/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="extras/style.css">
@@ -18,7 +18,7 @@
     </nav>
     <form method="post" action="login.php">
     <div class="container content">
-        <h1 class="hood">TuAnalytics</h1>
+        <h1 class="hood">Hanalistik</h1>
         <div class="loginBox">
             <input type="text" placeholder="kullanıcı adı" name="admin_login_name" class="login_input">
             <input type="password" placeholder="şifre" name="admin_login_password" class="login_input">
