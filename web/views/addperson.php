c29f1404fed71b1c0d6c9760490a569e1dc2a7a1
Yusuf Erdem Turan <76514562+erdemskywalker@users.noreply.github.com>
2026-06-03 22:06:51 +0300

    Update addperson.php

--- web/views/addperson.php
+++ web/views/addperson.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TuAnalytics</title>
+    <title>Hanalistik</title>
     <link rel="stylesheet" href="extras/bootstrap.min.css">
     <script src="extras/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="extras/style.css">
