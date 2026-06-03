e72f7325fd567dbeef3c690b149f9dc8bfa47295
Yusuf Erdem Turan <76514562+erdemskywalker@users.noreply.github.com>
2026-06-03 22:07:26 +0300

    Update logs.php

--- web/views/logs.php
+++ web/views/logs.php
@@ -1,6 +1,6 @@
 
 <?php
-$db=new PDO("sqlite:../TuAnalytics.db");
+$db=new PDO("sqlite:../Hanalistik.db");
 
 $query=$db->prepare("SELECT * FROM logs");
 $query->execute();
@@ -15,7 +15,7 @@ $rows = $query->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TuAnalytics</title>
+    <title>Hanalistik</title>
     <link rel="stylesheet" href="extras/bootstrap.min.css">
     <script src="extras/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="extras/style.css">
