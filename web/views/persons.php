c790aaddebb3582d4f8112d60b5b486e67298482
Yusuf Erdem Turan <76514562+erdemskywalker@users.noreply.github.com>
2026-06-03 22:08:03 +0300

    Update persons.php

--- web/views/persons.php
+++ web/views/persons.php
@@ -1,5 +1,5 @@
 <?php
-$db=new PDO("sqlite:../TuAnalytics.db");
+$db=new PDO("sqlite:../Hanalistik.db");
 
 $query=$db->prepare("SELECT * FROM persons");
 $query->execute();
@@ -14,7 +14,7 @@ $rows = $query->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TuAnalytics</title>
+    <title>Hanalistik</title>
     <link rel="stylesheet" href="extras/bootstrap.min.css">
     <script src="extras/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="extras/style.css">
@@ -24,7 +24,7 @@ $rows = $query->fetchAll(PDO::FETCH_ASSOC);
 <body>
     <nav class="navbar">
         <div class="container-fluid">
-            <a href="./">TuAnalytics</a>
+            <a href="./">Hanalistik</a>
             <div>
             <i onclick="window.location='logs';" class="fa-solid fa-clock-rotate-left"></i>
             <i onclick="window.location='logout.php';" class="fa-solid fa-arrow-right-from-bracket"></i>
